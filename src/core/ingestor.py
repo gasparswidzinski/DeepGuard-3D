0df1c7aa73456c73a031c157daa681309e63df6a
gasparswidzinski <gasparswidzinski@gmail.com>
2026-02-03 20:08:12 -0300

    modifing ingestor.py to use this new official data

--- src/core/ingestor.py
+++ src/core/ingestor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os   
+import rasterio
 
 class BathymetryIngestor:
     """
@@ -13,18 +14,8 @@ class BathymetryIngestor:
     def load_data(self) -> pd.DataFrame:
         """Carga datos desde un archivo .csv o .xyz"""
         
-        if not os.path.exists(self.file_path):
-            raise FileNotFoundError(f"No se encontro el archivo {self.file_path}")
         
-        print(f"[*] Cargando datos desde: {self.file_path}")
         
-        # Leo el archivo asumiendo formato: Longitud, Latitud, Profundidad
-        self.data = pd.read_csv(self.file_path, names=['longitude', 'latitude', 'depth'])
-        
-        print(f"[+] Exito al cargar {len(self.data)} registros.")
-        
-        return self.data
-    
     def get_statistics(self) -> dict:
         """Devuelve estadísticas básicas del relieve submarino"""
         if self.data is not None:
