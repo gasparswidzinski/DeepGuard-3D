a3d1a24ebd7b3d02ed26aa18fcd322dfe02b4c81
gasparswidzinski <gasparswidzinski@gmail.com>
2026-02-03 20:06:12 -0300

    OpenTopography Data put in data/raw to use real data

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from src.core.ingestor import BathymetryIngestor
-from src.visualization.visualizer import BathymetryVisualizer    
+from src.visualization.visualizer import BathymetryVisualizer
+from src.security.integrity import DataIntegrityManager
 import os
 
 def main():
+    print("--- DeepGuard 3D: Mission Control ---")
+    data_path = os.path.join("data", "raw", "output_SRTM15Plus.tif")
     
-    print("---- DeepGuard 3D Bathymetry Ingestion ----")
+    # 1. SECURITY LAYER
+    integrity = DataIntegrityManager(data_path)
+    current_hash = integrity.calculate_sha256()
+    print(f"[SEC] SHA-256 Fingerprint: {current_hash}")
     
-    #1.define the path to the raw data
-    data_path = os.path.join("data","raw","mock_seabed.csv")
+    # 2. INGESTION LAYER
+    ingestor = BathymetryIngestor(data_path)
+    df = ingestor.load_data()
     
-    
-    
-    #2. initialize the ingestor
-    try:
-        # Ingestion
-        ingestor = BathymetryIngestor(data_path)
-        df = ingestor.load_data()
-        
-        # Statistics
-        print("\n[i] Seabed Statistical Summary:")
-        print(ingestor.get_statistics())
-        
-        # Visualization
-        visualizer = BathymetryVisualizer(df)
-        visualizer.render_point_cloud()
+    # 3. VISUALIZATION LAYER
+    visualizer = BathymetryVisualizer(df)
+    visualizer.render_point_cloud()
 
-    except Exception as e:
-        print(f"[FATAL ERROR] {e}") 
-    
 if __name__ == "__main__":
     main()
