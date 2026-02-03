e03dddf71b2c2be36c958653b1f16b25d372cf6c
gasparswidzinski <gasparswidzinski@gmail.com>
2026-02-03 20:13:41 -0300

    changes working, lets fix some bugs

--- src/core/ingestor.py
+++ src/core/ingestor.py
@@ -13,7 +13,16 @@ class BathymetryIngestor:
         
     def load_data(self) -> pd.DataFrame:
         """Carga datos desde un archivo .csv o .xyz"""
-        
+        print(f"[*] Extracting real bathymetry from: {self.file_path}")
+        with rasterio.open(self.file_path) as dataset:
+            data = dataset.read(1)
+            # Filter out 'No Data' and land values if necessary
+            mask = (data != dataset.nodata ) & (~np.isnan(data))
+            rows,col = np.where(mask)
+            lons,lats = dataset.xy(rows,col)
+            depths = data[rows,col]
+            
+            return pd.DataFrame({'longitude': lons, 'latitude': lats, 'depth': depths})
         
         
     def get_statistics(self) -> dict:
